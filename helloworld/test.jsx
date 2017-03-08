574a94f3d2c9119496cbe12999df1cc2385283b6
gaomin <gaominwangyi@163.com>
2017-03-08 10:03:25 +0800

    test

---
+++ helloworld/test.jsx
@@ -0,0 +1,20 @@
+define(['react', 'react-dom'],function(React, ReactDOM){
+
+  var test = {
+     // init: function(){
+     //    // this.AppView = React.createClass({
+     //    //   render: function () {
+     //    //     return (
+     //    //       <div>
+     //    //         <p>Hello, React!</p>
+     //    //       </div>
+     //    //     );
+     //    //   }
+     //    // });
+     //    // ReactDOM.render(<this.AppView />, document.getElementById('example'));
+     // }
+  }
+
+  return test;
+
+});
